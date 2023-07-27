32383aab3baa7e0320ba1912bf2d916496343617
Leon Sulfierry <leon.bio0@hotmail.com>
2023-07-27 16:44:05 -0300

    Update classific_aac.py

--- classific_aac.py
+++ classific_aac.py
@@ -4,7 +4,7 @@ import csv
 
 # Abre o arquivo PDB e pega a estrutura
 parser = PDBParser(QUIET=True)
-structure = parser.get_structure("name", "./new_kleb_sub_align.pdb")
+structure = parser.get_structure("name", "./3c9t.pdb")
 
 # Classificação dos aminoácidos
 molecule_class = {
@@ -29,13 +29,13 @@ molecule_class = {
     "ASP": "polar com carga negativa",
     "GLU": "polar com carga negativa",
     "MG" : "cofator com carga positiva",
-    "HOH": "polar tendo H carga parcial positiva, e O carga parcial negativa",
+    "HOH": "polar carga parcial negativa",
     "ACP": "ATP",
     "TPS": "TMP"
 }
 
 # Selecione o ligante
-ligand_selection = ('A', 329)  # uma tupla com ('cadeia', número do resíduo)
+ligand_selection = ('A', 'TPS')  # uma tupla com ('cadeia', número do resíduo)
 ligand_residue = None
 
 for chain in structure[0]:
@@ -54,29 +54,37 @@ if ligand_residue is None:
     raise ValueError(f"Ligante {ligand_selection} não encontrado na estrutura")
 
 
-# Lista para armazenar os resíduos próximos
-near_residues = []
+# Dicionário para armazenar os resíduos próximos e a interação
+near_residues = {}
 
 # Verifica todos os átomos de todos os resíduos
 for chain in structure[0]:
     for residue in chain:
         if residue != ligand_residue:  # Ignora o ligante
+            min_distance = np.inf
+            interacting_atoms = None
             for atom in residue:
                 for ligand_atom in ligand_residue:
                     distance = atom - ligand_atom
-                    if distance <= 4.0:
-                        if residue not in near_residues:
-                            near_residues.append(residue)
-                        break  # Não precisa verificar os outros átomos deste resíduo
+                    if distance < min_distance:
+                        min_distance = distance  # Mantém a distância mínima
+                        interacting_atoms = (atom, ligand_atom)  # Mantém os átomos que produziram a distância mínima
+            if min_distance <= 4.0:
+                interaction = 'H-bond (1.5 to 2.7 Å)' if min_distance <= 2.9 else 'van der Waals (3.0 to 4.0 Å)'
+                if residue not in near_residues or near_residues[residue][0] == 'van der Waals (3.0 to 4.0 Å)':
+                    near_residues[residue] = (interaction, interacting_atoms)
 
 # Escreve os resíduos próximos em um arquivo csv
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Nome do aminoácido", "Número", "Classificação"])
-    print("{:<20} {:<10} {:<30}".format("Aminoácido", "Número", "Classificação"))   
-    for residue in near_residues:
+    writer.writerow(["Nome do aminoácido", "Número", "Classificação", "Interação", "Átomos Interagindo"])
+    print("{:<20} {:<10} {:<30} {:<15} {:<20}".format("Aminoácido", "Número", "Classificação", "Interação", "Átomos Interagindo"))   
+    for residue, (interaction, atoms) in near_residues.items():
         aa_name = residue.get_resname()
         aa_num = residue.get_id()[1]
         aa_class = molecule_class.get(aa_name, "desconhecido")
-        writer.writerow([aa_name, aa_num, aa_class])
-        print("{:<20} {:<10} {:<30}".format(aa_name, aa_num, aa_class))
+        atom1 = atoms[0].get_name() + "(" + residue.get_resname() + ")"
+        atom2 = atoms[1].get_name() + "(" + ligand_residue.get_resname() + ")"
+        interacting_atoms_str = "{:<10}-{:>10}".format(atom1, atom2)  # Ajusta o tamanho dos campos dos átomos
+        writer.writerow([aa_name, aa_num, aa_class, interaction, interacting_atoms_str])
+        print("{:<20} {:<10} {:<30} {:<15} {:<20}".format(aa_name, aa_num, aa_class, interaction, interacting_atoms_str))
