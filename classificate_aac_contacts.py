a1444b52989cbf2e0d43a67f4f185aa097b59bd0
Leon Sulfierry <leon.bio0@hotmail.com>
2023-08-07 19:15:26 -0300

    Update classificate_aac_contacts.py

--- classificate_aac_contacts.py
+++ classificate_aac_contacts.py
@@ -17,7 +17,7 @@ parser = PDBParser(QUIET=True)
 structure = parser.get_structure("name", input_pdb)
 
 
-# Amino acids classification
+# Amino acids and nucleic acid bases classification
 molecule_class = {
     # Amino Acids
     "ALA": "hydrophobic",
@@ -61,7 +61,6 @@ molecule_class = {
 if molecule_select not in molecule_class:
     molecule_class[molecule_select] = molecule_select
 
-
 # Ligand selection
 # a tuple with ('string', residue number)
 ligand_residue = None
@@ -95,6 +94,26 @@ near_residues = []
 # List to store the positions of the next residues
 near_residues_positions = set()  
 
+# Define a function to check for potential hydrogen bonds
+def is_interaction(atom1, atom2, residue_name, distance):
+    hydrogen_bond_atoms = ["O", "N", "F"]
+    ionic_interactions = {
+        "NA": ["F", "Cl", "Br", "I", "O", "S"],
+        "MG": ["F", "Cl", "Br", "I", "O", "S"],
+        "K": ["F", "Cl", "Br", "I", "O", "S"],
+        "CA": ["F", "Cl", "Br", "I", "O", "S"],
+        #"HOH": ["F", "Cl", "Br", "I", "O", "S"]
+    }
+    
+    if distance < 3.0:
+        if atom1.get_name().startswith(tuple(hydrogen_bond_atoms)) or atom2.get_name().startswith(tuple(hydrogen_bond_atoms)):
+            if residue_name in ionic_interactions:
+                if atom1.get_name().startswith(tuple(ionic_interactions[residue_name])) or atom2.get_name().startswith(tuple(ionic_interactions[residue_name])):
+                    return "Ionic"
+            return "True"
+    return "False"
+
+
 
 # Check all atoms of all residues
 for chain in structure[0]:
@@ -120,16 +139,38 @@ near_residues.sort(key=lambda x: x[1])
 # Write the residuals to a csv file
 with open(output_name, 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["Amino acid", "Number", "Chain", "Classification", "Nearby atoms", "Distance Å"])  # Add "Chain"
-    columns = ["Amino acid", "Number", "Chain", "Classification", "Nearby atoms", "Distance Å"]  # Add "Chain"
-    print("{:^20} {:^10} {:^5} {:^30} {:^20} {:^10}".format(*columns))   # Add another column for "Chain"
+    # Change the order of columns here
+    writer.writerow(["Molecule", "Classification", "Number", 
+                     "Chain", "Nearby atoms", "Distance Å", "H bond"]) 
+    
+    columns = ["Molecule", "Classification", "Number", 
+               "Chain", "Nearby atoms", "Distance Å", "H bond"] 
+    
+    print("{:^20} {:^30} {:^10} {:^5} {:^20} {:^10} {:^6}".format(*columns))
     for residue, distance, atoms in near_residues:
         aa_name = residue.get_resname()
         aa_num = residue.get_id()[1]
         chain_id = residue.get_parent().get_id()  # Get chain ID
-        aa_class = molecule_class.get(aa_name, "unknown")
+
+        if aa_name in ["NA", "MG", "K", "CA", "HOH"]:
+            aa_class = "cofactor"
+        else:
+            aa_class = molecule_class.get(aa_name, "unknown")
+
         atom1 = atoms[0].get_name() + "(" + residue.get_resname() + ")"
         atom2 = atoms[1].get_name() + "(" + ligand_residue.get_resname() + ")"
         nearby_atoms_str = "{:<10}-{:>10}".format(atom1, atom2)
-        writer.writerow([aa_name, aa_num, chain_id, aa_class, nearby_atoms_str, round(distance, 2)])  # Add chain_id
-        print("{:^20} {:^10} {:^5} {:^30} {:^20} {:^10.2f}".format(aa_name, aa_num, chain_id, aa_class, nearby_atoms_str, distance))  # Add chain_id
+        
+        # Check for hydrogen bond
+        h_bond = is_interaction(atoms[0], atoms[1], residue.get_resname(), distance)
+        
+        # If the result is "probable", set h_bond to "probable". Otherwise, set it to "True" or "False".
+        #h_bond = "probable" if h_bond_check == "probable" else ("True" if h_bond_check else "False")
+
+
+        # Change the order of written rows here to match column header order
+        writer.writerow([aa_name, aa_class, aa_num, 
+                         chain_id, nearby_atoms_str, round(distance, 2), h_bond]) 
+        
+        print("{:^20} {:^30} {:^10} {:^5} {:^20} {:^10.2f} {:^6}".format(
+            aa_name, aa_class, aa_num, chain_id, nearby_atoms_str, distance, h_bond))
