b8d6a047254158854f5a90fc8c774a3703bc7863
sulfierry <leon.bio0@hotmail.com>
2023-09-05 17:30:24 -0300

    vox

--- vox/vox.py
+++ vox/vox.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,71 +23,40 @@ def plot_voxel(voxel):
 
     plt.show()
 
-# Parser for PDB structure with consideration for protein structure, ligands, and cofactors
-def parse_pdb(pdb_file):
-    """
-    Parses a PDB file to extract chains, cofactors, ligands, and atom details.
+import numpy as np
 
-    Parameters:
-        pdb_file (str): Path to the PDB file.
-
-    Returns:
-        dict: A dictionary containing lists:
-              - 'chains': List of ATOM details.
-              - 'cofactors': List of HETATM details matching cofactor names.
-              - 'ligands': List of HETATM details matching ligand names.
-    """
+# Função para analisar o arquivo PDB
+def parse_pdb(pdb_file_path):
+    with open(pdb_file_path, 'r') as file:
+        pdb_data = file.readlines()
     
-    def extract_atom_details(line):
-        """Helper function to extract atom details from a line."""
-        try:
-            return {
-                'serial_number': int(line[6:11].strip()),
+    parsed_data = {
+        'chains': [],
+        'cofactors': [],
+        'ligands': []
+    }
+
+    for line in pdb_data:
+        if line.startswith("ATOM"):
+            parsed_atom = {
                 'name': line[12:16].strip(),
-                'alt_loc': line[16].strip(),
+                'altLoc': line[16].strip(),
                 'res_name': line[17:20].strip(),
                 'chain_id': line[21].strip(),
                 'res_seq': int(line[22:26].strip()),
                 'icode': line[26].strip(),
-                'coord': [
-                    float(line[30:38]),
-                    float(line[38:46]),
-                    float(line[46:54])
-                ],
-                'occupancy': float(line[54:60].strip()),
-                'temp_factor': float(line[60:66].strip()),
+                'coord': [float(line[30:38]), float(line[38:46]), float(line[46:54])],
+                'occupancy': float(line[54:60]),
+                'temp_factor': float(line[60:66]),
                 'element': line[76:78].strip(),
                 'charge': line[78:80].strip()
             }
-        except ValueError:
-            print(f"Problem with line: {line}")
-            raise
+            parsed_data['chains'].append(parsed_atom)
+        elif line.startswith(("HETATM", "ANISOU", "TER", "CONECT")):
+            # Processa outras linhas conforme necessário
+            pass  # Placeholder para processamento adicional
 
-
-    chains = []
-    cofactors = []
-    ligands = []
-
-    cofactor_names = ["MG", "ZN", "CA", "K", "NA", "FE", "CL", "HOH"]
-    ligand_names = ["ACP", "TPS", "TMP", "TPP", "LIG"]
-
-    with open(pdb_file, 'r') as f:
-        for line in f:
-            if line.startswith('ATOM'):
-                chains.append(extract_atom_details(line))
-            elif line.startswith('HETATM'):
-                residue_name = line[17:20].strip()
-                details = extract_atom_details(line)
-                if residue_name in cofactor_names:
-                    cofactors.append(details)
-                elif residue_name in ligand_names:
-                    ligands.append(details)
-
-    return {
-        'chains': chains,
-        'cofactors': cofactors,
-        'ligands': ligands
-    }
+    return parsed_data
 
 # Função para converter coordenadas do PDB para índices de voxel
 def coord_to_voxel(coord, origin, grid_size):
@@ -97,33 +67,62 @@ def coord_to_voxel(coord, origin, grid_size):
     return voxel_coord
 
 # Função para converter o PDB (no formato de dicionário) para um voxel grid
-def pdb_to_voxel_with_info(parsed_pdb):
+def pdb_to_voxel_atom(parsed_pdb):
     voxel_grid = np.zeros(grid_dim, dtype= int)
-    info_grid = np.empty(grid_dim, dtype=object)
+    atom_info_grid = np.empty(grid_dim, dtype=object)
 
-    # Iterar sobre todos os átomos e converter suas coordenadas para coordenadas de voxel
     for atom_section in ["chains", "cofactors", "ligands"]:
         for atom_details in parsed_pdb[atom_section]:
             voxel_coord = coord_to_voxel(np.array(atom_details["coord"]), origin, grid_size)
             if voxel_coord is not None:
                 voxel_grid[tuple(voxel_coord)] = 1
-                info_grid[tuple(voxel_coord)] = atom_details
+                atom_info_grid[tuple(voxel_coord)] = atom_details
 
-    return voxel_grid, info_grid
+    return voxel_grid, atom_info_grid
+
+# Função para converter o PDB (no formato de dicionário) para uma voxel grid representando aminoácidos
+def pdb_to_voxel_residue(parsed_pdb):
+    voxel_grid = np.zeros(grid_dim, dtype=int)
+    residue_info_grid = np.empty(grid_dim, dtype=object)
+
+    seen_residues = set()
+
+    for atom_section in ["chains", "cofactors", "ligands"]:
+        for atom_details in parsed_pdb[atom_section]:
+            residue_identifier = (atom_details['chain_id'], atom_details['res_seq'], atom_details['icode'])
+            if residue_identifier not in seen_residues:
+                seen_residues.add(residue_identifier)
+                if atom_details['name'] == 'CA':
+                    voxel_coord = coord_to_voxel(np.array(atom_details["coord"]), origin, grid_size)
+                    if voxel_coord is not None:
+                        voxel_grid[tuple(voxel_coord)] = 1
+                        residue_info = {
+                            'res_name': atom_details['res_name'],
+                            'chain_id': atom_details['chain_id'],
+                            'res_seq': atom_details['res_seq'],
+                            'icode': atom_details['icode']
+                        }
+                        residue_info_grid[tuple(voxel_coord)] = residue_info
+
+    return voxel_grid, residue_info_grid
 
 
 
-# Definição do tamanho e origem da grade (ajuste conforme necessário)
-grid_dim = [64, 64, 64]  # Grid dimensions (x, y, z)
-grid_size = 1.0  # Size of each voxel in Ångström
-origin = np.array([17.773, 63.285, 121.743])  # Origin of the grid in the PDB coordinate space
+# Definição do tamanho e origem da grade
+grid_dim = [64, 64, 64]
+grid_size = 1.0
+origin = np.array([17.773, 63.285, 121.743])
 
+parsed_pdb = parse_pdb("./3c9t.pdb")  
+atom_voxel_grid, atom_info_grid = pdb_to_voxel_atom(parsed_pdb)
+aac_voxel_grid, acc_info_grid = pdb_to_voxel_residue(parsed_pdb)
 
-# Chamada de teste
-parsed_pdb = parse_pdb("./3c9t.pdb")  # Substitua "your_pdb_file.pdb" pelo caminho do seu arquivo PDB
-voxel_grid, info_grid = pdb_to_voxel_with_info(parsed_pdb)
-plot_voxel(voxel_grid)
+print(atom_info_grid.shape)
+#plot_voxel(atom_voxel_grid)
+# plot_voxel(aac_voxel_grid)
+"""armazenar informações adicionais sobre cada átomo quando estiver preenchendo sua grade de voxels."""
 
+# atom_vector = atom_voxel_grid.flatten()
+# aac_vector = aac_voxel_grid.flatten()
+# print(atom_vector.shape, aac_vector.shape)
 
-
-"""armazenar informações adicionais sobre cada átomo quando estiver preenchendo sua grade de voxels"""
