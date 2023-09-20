c2a7a68c7e9ef183638583ea0ccca776df481944
sulfierry <leon.bio0@hotmail.com>
2023-09-20 16:09:56 -0300

    API

--- FOLDSEEK_API/foldseek_API.py
+++ FOLDSEEK_API/foldseek_API.py
@@ -1,142 +1,79 @@
 import requests
+import time
 
-BASE_URL = "https://search.mmseqs.com/api"
-
-def check_ticket_status(ticket_id):
-    # URL para verificar o status do trabalho
-    url = f"{BASE_URL}/ticket/{ticket_id}"
+class FoldseekAPI:
     
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Erro ao consultar o status. Status code:", response.status_code)
-        return None
+    def __init__(self):
+        self.MMSEQS2_BASE_URL = "https://search.mmseqs.com/api"
+        self.FOLDSEEK_BASE_URL = "https://search.foldseek.com/api"
 
-def get_results(ticket_id, entry):
-    # URL para obter os resultados
-    url = f"{BASE_URL}/result/{ticket_id}/{entry}"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Erro ao obter os resultados. Status code:", response.status_code)
-        return None
+    def check_ticket_status(self, ticket_id):
+        url = f"{self.MMSEQS2_BASE_URL}/ticket/{ticket_id}"
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("Erro ao consultar o status. Status code:", response.status_code)
+            return None
 
-# Usando as funções
-ticket_id = 'YOUR_TICKET_ID'
-status = check_ticket_status(ticket_id)
-if status and status.get('status') == "COMPLETED":
-    # Assumindo 'entry' como um parâmetro necessário. Modifique conforme necessário.
-    entry = 'YOUR_ENTRY'
-    results = get_results(ticket_id, entry)
-    print(results)
-else:
-    print("O trabalho ainda não está concluído ou ocorreu um erro.")
+    def download_results(self, ticket_id):
+        url = f"{self.MMSEQS2_BASE_URL}/result/download/{ticket_id}"
+        response = requests.get(url, stream=True)
+        with open(f"results_{ticket_id}.pdb", 'wb') as fd:
+            for chunk in response.iter_content(chunk_size=128):
+                fd.write(chunk)
+        print("Download concluído.")
 
+    def submit_to_foldseek(self, file_path):
+        url = f"{self.FOLDSEEK_BASE_URL}/ticket"
+        databases = [
+            'afdb50', 'afdb-swissprot', 'afdb-proteome', 'cath50', 
+            'mgnify_esm30', 'pdb100', 'gmgcl_id'
+        ]
 
+        data = {'mode': '3diaa'}
+        for db in databases:
+            data.setdefault('database[]', []).append(db)
 
+        with open(file_path, 'rb') as f:
+            files = {'q': (file_path, f)}
+            response = requests.post(url, files=files, data=data)
 
-def submit_to_foldseek(file_path):
-    # URL para a API
-    url = "https://search.foldseek.com/api/ticket"
+        if response.status_code == 200:
+            print("Arquivo enviado com sucesso!")
+            return response.json()
+        else:
+            print("Erro ao enviar o arquivo. Status code:", response.status_code)
+            return None
 
-    # Definição dos parâmetros e arquivos a serem enviados
-    databases = [
-        'afdb50', 
-        'afdb-swissprot', 
-        'afdb-proteome', 
-        'cath50', 
-        'mgnify_esm30', 
-        'pdb100', 
-        'gmgcl_id'
-    ]
-    files = {'q': (file_path, open(file_path, 'rb'))}
-    data = {
-        'mode': '3diaa',
-    }
-    for db in databases:
-        data['database[]'] = db
-
-    # Realizando a requisição POST
-    response = requests.post(url, files=files, data=data)
-
-    # Verifica se a requisição foi bem-sucedida
-    if response.status_code == 200:
-        print("Arquivo enviado com sucesso!")
-        return response.json()
-    else:
-        print("Erro ao enviar o arquivo. Status code:", response.status_code)
-        return None
+    def wait_for_completion(self, ticket_id, polling_interval=60):
+        while True:
+            status = self.check_ticket_status(ticket_id)
+            if status and status.get('status') == "COMPLETED":
+                return True
+            elif status and status.get('status') == "ERROR":
+                print("Houve um erro no processamento.")
+                return False
+            else:
+                time.sleep(polling_interval)
 
 
-def check_foldseek_status(ticket_id):
-    # URL para verificar o status do trabalho
-    url = f"https://search.foldseek.com/api/status/{ticket_id}"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Erro ao consultar o status. Status code:", response.status_code)
-        return None
+# Exemplo de uso da classe:
 
-import requests
-
-BASE_URL = "https://search.mmseqs.com/api"
-
-def check_ticket_status(ticket_id):
-    # URL para verificar o status do trabalho
-    url = f"{BASE_URL}/ticket/{ticket_id}"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Erro ao consultar o status. Status code:", response.status_code)
-        return None
-
-def get_results(ticket_id, entry):
-    # URL para obter os resultados
-    url = f"{BASE_URL}/result/{ticket_id}/{entry}"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print("Erro ao obter os resultados. Status code:", response.status_code)
-        return None
-
-
+api = FoldseekAPI()
 
 # Teste
 file_path = "./3c9t.pdb"
-result = submit_to_foldseek(file_path)
+result = api.submit_to_foldseek(file_path)
 if result:
     print(result)
 
-print(result['id'])
+ticket_id = result['id']
+print(ticket_id)
 
-# Usando a função para consultar o status
-#status = check_foldseek_status(result['id'])
-#if status:
-#    print(status)
-
-
-
-# Usando as funções
-# ticket_id = 'YOUR_TICKET_ID'
-status = check_ticket_status(result['id'])
-if status and status.get('status') == "COMPLETED":
-    # Assumindo 'entry' como um parâmetro necessário. Modifique conforme necessário.
-    entry = 'YOUR_ENTRY'
-    results = get_results(result['id'], entry)
-    print(results)
+# Aguarda a conclusão
+if api.wait_for_completion(ticket_id):
+    api.download_results(ticket_id)
 else:
-    print("O trabalho ainda não está concluído ou ocorreu um erro.")
+    print("O trabalho não foi concluído com sucesso.")
