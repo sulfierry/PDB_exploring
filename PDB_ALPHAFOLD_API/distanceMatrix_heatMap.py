315388f345e38fd67f6ee2b006ee6b825cab9bb4
sulfierry <leon.bio0@hotmail.com>
2023-09-22 18:39:59 -0300

    DM_HM

--- PDB_ALPHAFOLD_API/distanceMatrix_heatMap.py
+++ PDB_ALPHAFOLD_API/distanceMatrix_heatMap.py
@@ -1,9 +1,10 @@
+# Consolidating the code into one block
+
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Função para processar cada linha do arquivo CSV
+# Function to process each line of the CSV file
 def process_line_v5(line, total_columns):
     """Extract the sequence name and the distance values from a line using the provided rule."""
     parts = line.split(",")
@@ -11,46 +12,43 @@ def process_line_v5(line, total_columns):
     values = []
     i = 1
     while i < len(parts):
-        # Se o valor atual é 0 e é seguido por uma vírgula, é um valor zero completo
         if parts[i] == '0' and i < len(parts) - 1 and parts[i+1] == '0':
             values.append(0.0)
             i += 1
-        # Se o valor atual é 0 e é seguido por um número, é um valor decimal
         elif parts[i] == '0' and i < len(parts) - 1:
             values.append(float(f"0.{parts[i+1]}"))
             i += 2
         else:
             i += 1
-            
-    # Garante que o número de valores corresponda ao total de colunas
     while len(values) < total_columns:
         values.append(0.0)
-
     return sequence_name, values
 
-# Determine o número de colunas pelo número de nomes de sequências
-total_columns = len(sequence_names_v4)
+# Read the CSV file to determine the number of columns
+with open('./distance_matrix.csv', 'r') as f:
+    sequence_names = [line.strip().split(",")[0] for line in f if line.strip()]
 
-sequence_names_v5 = []
-data_v5 = []
+total_columns = len(sequence_names)
 
-with open('/distance_matrix.csv', 'r') as f:
+data = []
+
+with open('./distance_matrix.csv', 'r') as f:
     for line in f:
         line = line.strip()
         if not line:
             continue
         sequence_name, values = process_line_v5(line, total_columns)
-        sequence_names_v5.append(sequence_name)
-        data_v5.append(values)
+        data.append(values)
 
-# Construa o DataFrame final usando os novos dados
-final_distance_matrix_v6 = pd.DataFrame(data_v5, columns=sequence_names_v5, index=sequence_names_v5)
+# Construct the DataFrame using the processed data
+distance_matrix = pd.DataFrame(data, columns=sequence_names, index=sequence_names)
 
-# Remova a primeira linha que contém apenas zeros
-final_distance_matrix_v7 = final_distance_matrix_v6.drop(final_distance_matrix_v6.index[0])
+# Remove the first row which contains only zeros
+distance_matrix = distance_matrix.drop(distance_matrix.index[0])
 
-# Visualize a matriz de distância atualizada como um heatmap
+# Display the first 10x10 values of the matrix as a heatmap
 plt.figure(figsize=(12, 10))
-sns.heatmap(final_distance_matrix_v7.iloc[:11, :11], cmap="YlGnBu", annot=True, fmt=".3f", linewidths=.5)
-plt.title("Matriz de Distância Atualizada (Primeiros 11x11 Valores)")
+sns.heatmap(distance_matrix.iloc[:11, :11], cmap="YlGnBu", annot=True, fmt=".3f", linewidths=.5)
+plt.title("Matriz de Distância (Primeiros 10x10 Valores)")
 plt.show()
+
